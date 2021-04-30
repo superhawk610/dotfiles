595714da56c74e8a81096457ea2ed22701a3f69d
Aaron Ross <superhawk610@gmail.com>
2021-04-30 10:57:48 -0700

    add GitHub Codespaces bootstrap

---
+++ bootstrap.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -eo pipefail
+
+# Github Codespaces bootstrap
+
+DOTFILES=$(dirname $1)
+echo "linking against $DOTFILES"
+
+echo "setting up home directory"
+mkdir -p "${HOME}/.zshrc.d"
+mkdir -p "${HOME}/.config/nvim"
+
+echo "linking files"
+ln -S "${DOTFILES}/.gitconfig"              "${HOME}/.gitconfig"
+ln -S "${DOTFILES}/.zshrc"                  "${HOME}/.zshrc"
+ln -S "${DOTFILES}/.zshrc.d/00-minimal.zsh" "${HOME}/.zshrc.d/00-minimal.zsh"
+ln -S "${DOTFILES}/.config/nvim/init.vim"   "${HOME}/.config/nvim/init.vim"
+
+echo "done!"
+

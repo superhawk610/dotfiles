6bec6be93def07b2bf2f1c75732a51d46e5876b1
Aaron Ross <superhawk610@gmail.com>
2021-04-30 10:52:53 -0700

    split zsh config

---
+++ .zshrc.d/10-full.zsh
@@ -0,0 +1,62 @@
+# copy to ~/.zshrc.d to enable
+
+export ZSH="$HOME/.oh-my-zsh"
+# ZSH_THEME="starship" (set at end of .zshrc)
+
+plugins=(
+  wd
+  git
+  zsh-syntax-highlighting
+  zsh-autosuggestions
+  asdf
+)
+
+source $ZSH/oh-my-zsh.sh
+
+# path
+export PATH=$PATH:~/Library/Python/3.9/bin
+
+# alias -----------------------
+
+## general
+alias ls='exa'
+alias cat='bat'
+
+## ultralist
+alias ul='ultralist l'
+alias ule='ultralist edit'
+alias ula='ultralist add'
+alias ulab='ultralist add +backlog'
+
+## git
+unalias gp # set by oh-my-zsh
+
+# setup -----------------------
+
+# clojure
+# export LEIN_USE_BOOTCLASSPATH=no # fix for ultra
+
+# java
+export PATH="~/.jenv/bin:$PATH"
+eval "$(jenv init -)"
+
+# ruby
+# export PATH=$PATH:~/.gem/ruby/2.6.0/bin
+
+# p10k
+# [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+# fzf
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+# nvm (disabled in favor of asdf)
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# direnv
+eval "$(direnv hook zsh)"
+
+# starship (prompt)
+eval "$(starship init zsh)"
+

db58b7284021c8185072e3bac1472bbe6e6888ce
Aaron Ross <superhawk610@gmail.com>
2022-03-28 20:22:23 -0700

    fix nvim-tree config

--- .config/nvim/lua/plugins/filetree.lua
+++ .config/nvim/lua/plugins/filetree.lua
@@ -1,6 +1,4 @@
-local tree_cb = require('nvim-tree.config').nvim_tree_callback
 local g = vim.g
-
 local M = {}
 
 g.nvim_tree_auto_ignore_ft = {'startify'}
@@ -22,11 +20,11 @@ g.nvim_tree_icons = {
   default = '',
   symlink = '',
   git = {
-    unstaged = '✗',
+    unstaged = 'U',
     staged = '✓',
     unmerged = '',
     renamed = '➜',
-    untracked = 'U',
+    untracked = '?',
     deleted = 'D',
     ignored = '',
   },
@@ -49,42 +47,23 @@ function M.toggle_dir()
 
   local node = lib.get_node_at_cursor()
   if not node then return end
-  if not node.entries then return end
+  if not node.nodes then return end
 
   lib.expand_or_collapse(node)
 end
 
 function M.edit_file()
   local lib = require('nvim-tree.lib')
+  local open_file = require('nvim-tree.actions.open-file')
 
   local node = lib.get_node_at_cursor()
   if not node then return end
 
-  if node.link_to and not node.entries then
-    lib.open_file('edit', node.link_to)
-  elseif not node.entries then
-    lib.open_file('edit', node.absolute_path)
+  if node.link_to and not node.nodes then
+    open_file.fn('edit', node.link_to)
+  elseif not node.nodes then
+    open_file.fn('edit', node.absolute_path)
   end
-end
-
-function M.close_all()
-  local lib = require('nvim-tree.lib')
-  local view = require('nvim-tree.view')
-
-  local function iter(entries)
-    for _, node in ipairs(entries) do
-      if node.entries and node.open then
-        iter(node.entries)
-        node.open = false
-      end
-    end
-  end
-
-  iter(lib.Tree.entries)
-  lib.redraw()
-
-  -- move cursor back to top of tree
-  view.set_cursor({2, 0})
 end
 
 require('nvim-tree').setup {
@@ -109,34 +88,34 @@ require('nvim-tree').setup {
     mappings = {
       custom_only = true,
       list = {
-        { key = {'<CR>', 'o'}, cb = tree_cb('edit') },
+        { key = {'<CR>', 'o'}, action = 'edit' },
         { key = '<2-LeftMouse>', cb = local_cb('edit_file') },
         { key = '<LeftRelease>', cb = local_cb('toggle_dir') },
-        { key = 'C', cb = tree_cb('cd') },
-        { key = {'-', 'u'}, cb = tree_cb('dir_up') },
-        { key = 'X', cb = local_cb('close_all') },
-        { key = '<BS>', cb = tree_cb('close_node') },
-        { key = 'P', cb = tree_cb('parent_node') },
-        { key = '<', cb = tree_cb('prev_sibling') },
-        { key = '>', cb = tree_cb('next_sibling') },
-        { key = 'K', cb = tree_cb('first_sibling') },
-        { key = 'J', cb = tree_cb('last_sibling') },
-        { key = '<C-v>', cb = tree_cb('vsplit') },
-        { key = '<C-x>', cb = tree_cb('split') },
-        { key = '<C-t>', cb = tree_cb('tabnew') },
-        { key = 'I', cb = tree_cb('toggle_ignored') },
-        { key = 'H', cb = tree_cb('toggle_dotfiles') },
-        { key = 'a', cb = tree_cb('create') },
-        { key = 'd', cb = tree_cb('remove') },
-        { key = 'r', cb = tree_cb('rename') },
-        { key = '<C-r>', cb = tree_cb('full_rename') },
-        { key = 'x', cb = tree_cb('cut') },
-        { key = 'c', cb = tree_cb('copy') },
-        { key = 'p', cb = tree_cb('paste') },
-        { key = 'r', cb = tree_cb('rename') },
-        { key = 'R', cb = tree_cb('refresh') },
-        { key = 'q', cb = tree_cb('close') },
-        { key = '?', cb = tree_cb('toggle_help') },
+        { key = 'C', action = 'cd' },
+        { key = {'-', 'u'}, action = 'dir_up' },
+        { key = 'X', action = 'collapse_all' },
+        { key = '<BS>', action = 'close_node' },
+        { key = 'P', action = 'parent_node' },
+        { key = '<', action = 'prev_sibling' },
+        { key = '>', action = 'next_sibling' },
+        { key = 'K', action = 'first_sibling' },
+        { key = 'J', action = 'last_sibling' },
+        { key = '<C-v>', action = 'vsplit' },
+        { key = '<C-x>', action = 'split' },
+        { key = '<C-t>', action = 'tabnew' },
+        { key = 'I', action = 'toggle_ignored' },
+        { key = 'H', action = 'toggle_dotfiles' },
+        { key = 'a', action = 'create' },
+        { key = 'd', action = 'remove' },
+        { key = 'r', action = 'rename' },
+        { key = '<C-r>', action = 'full_rename' },
+        { key = 'x', action = 'cut' },
+        { key = 'c', action = 'copy' },
+        { key = 'p', action = 'paste' },
+        { key = 'r', action = 'rename' },
+        { key = 'R', action = 'refresh' },
+        { key = 'q', action = 'close' },
+        { key = '?', action = 'toggle_help' },
       },
     },
   },
